e9f1ffa4d1d8f9e4f21016d677eac7ae85193456
Majoxs <majoxs@gmail.com>
2021-08-23 14:20:02 +0500

    feat(script.js):Completed the assigned tasks

--- script/script.js
+++ script/script.js
@@ -1,38 +1,95 @@
+/* eslint-disable indent */
 /* eslint-disable strict */
 /* eslint-disable prefer-arrow-callback */
+
 window.addEventListener('DOMContentLoaded', function () {
-	'use strict';
+    'use strict';
 
-	//Timer
-	function countTimer(deadline) {
-		const timerHours = document.querySelector('#timer-hours'),
-			timerMinutes = document.querySelector('#timer-minutes'),
-			timerSeconds = document.querySelector('#timer-seconds');
+    //Timer
+    function countTimer(deadline) {
+        const timerHours = document.querySelector('#timer-hours'),
+            timerMinutes = document.querySelector('#timer-minutes'),
+            timerSeconds = document.querySelector('#timer-seconds');
 
-		function getTimeRemaining() {
-			const dateStop = new Date(deadline).getTime(),
-				dateNow = new Date().getTime(),
-				timeRemaining = (dateStop - dateNow) / 1000,
-				seconds = Math.floor(timeRemaining % 60),
-				minutes = Math.floor((timeRemaining / 60) % 60),
-				hours = Math.floor(timeRemaining / 60 / 60);
-			return { timeRemaining, hours, minutes, seconds };
-		}
+        function getTimeRemaining() {
+            const dateStop = new Date(deadline).getTime(),
+                dateNow = new Date().getTime(),
+                timeRemaining = (dateStop - dateNow) / 1000,
+                seconds = Math.floor(timeRemaining % 60),
+                minutes = Math.floor((timeRemaining / 60) % 60),
+                hours = Math.floor(timeRemaining / 60 / 60);
+            return { timeRemaining, hours, minutes, seconds };
+        }
 
-		function updateClock() {
-			const timer = getTimeRemaining();
+        function updateClock() {
+            const timer = getTimeRemaining();
 
-			timerHours.textContent = timer.hours;
-			timerMinutes.textContent = timer.minutes;
-			timerSeconds.textContent = timer.seconds;
+            timerHours.textContent = timer.hours;
+            timerMinutes.textContent = timer.minutes;
+            timerSeconds.textContent = timer.seconds;
 
-			if (timer.timeRemaining > 0) {
-				setTimeout(updateClock, 1000);
-			}
-		}
-		updateClock();
-	}
+            const idInterval = setInterval(updateClock, 1000);
 
-	countTimer('24 august 2021');
+            setTimeout(function () {
+                clearInterval(idInterval);
+            }, timer.timeRemaining);
+
+            if (timer.seconds < 10) {
+                timerSeconds.textContent = '0' + timer.seconds;
+            }
+            if (timer.minutes < 10) {
+                timerMinutes.textContent = '0' + timer.minutes;
+            }
+            if (timer.hours < 10) {
+                timerHours.textContent = '0' + timer.hours;
+            }
+
+            if (timer.timeRemaining <= 0) {
+                timerHours.textContent = '00';
+                timerMinutes.textContent = '00';
+                timerSeconds.textContent = '00';
+            }
+        }
+        updateClock();
+    }
+
+    countTimer('24 august 2021');
 
 });
+
+// window.addEventListener('DOMContentLoaded', function () {
+// 	'use strict';
+
+// 	//Timer
+// 	function countTimer(deadline) {
+// 		const timerHours = document.querySelector('#timer-hours'),
+// 			timerMinutes = document.querySelector('#timer-minutes'),
+// 			timerSeconds = document.querySelector('#timer-seconds');
+
+// 		function getTimeRemaining() {
+// 			const dateStop = new Date(deadline).getTime(),
+// 				dateNow = new Date().getTime(),
+// 				timeRemaining = (dateStop - dateNow) / 1000,
+// 				seconds = Math.floor(timeRemaining % 60),
+// 				minutes = Math.floor((timeRemaining / 60) % 60),
+// 				hours = Math.floor(timeRemaining / 60 / 60);
+// 			return { timeRemaining, hours, minutes, seconds };
+// 		}
+
+// 		function updateClock() {
+// 			const timer = getTimeRemaining();
+
+// 			timerHours.textContent = timer.hours;
+// 			timerMinutes.textContent = timer.minutes;
+// 			timerSeconds.textContent = timer.seconds;
+
+// 			if (timer.timeRemaining > 0) {
+// 				setTimeout(updateClock, 1000);
+// 			}
+// 		}
+// 		updateClock();
+// 	}
+
+// 	countTimer('24 august 2021');
+
+// });
